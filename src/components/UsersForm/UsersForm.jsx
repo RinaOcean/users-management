b67f3edb99c6c432406e65f7bf1436585da91532
RinaOcean <antyfifa@gmail.com>
2021-10-31 14:28:05 +0200

    add some styles on form

--- src/components/UsersForm/UsersForm.jsx
+++ src/components/UsersForm/UsersForm.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
 import { nanoid } from 'nanoid';
-import './UsersForm';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../../redux/users';
+
+import css from './UsersForm.module.css';
 
 export default function UsersForm() {
 
@@ -40,87 +41,89 @@ export default function UsersForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <label htmlFor={firstNameId}>First Name</label>
-      <input
-        type="text"
-        name="firstName"
-        required
-        placeholder="Your first name"
-        value={first_name}
-        onChange={event => setFirst_name(event.target.value)}
-        id={firstNameId}
-      />
+    <div className={css.formWrapper}>
+      <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+        <label htmlFor={firstNameId}>First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          required
+          placeholder="Your first name"
+          value={first_name}
+          onChange={event => setFirst_name(event.target.value)}
+          id={firstNameId}
+        />
 
-      <label htmlFor={lastNameId}>Last Name</label>
-      <input
-        type="text"
-        name="lastName"
-        required
-        placeholder="Your last name"
-        value={last_name}
-        onChange={event => setLast_name(event.target.value)}
-        id={lastNameId}
-      />
+        <label htmlFor={lastNameId}>Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          required
+          placeholder="Your last name"
+          value={last_name}
+          onChange={event => setLast_name(event.target.value)}
+          id={lastNameId}
+        />
 
-      <label htmlFor={birth_dateId}>Date of birth</label>
-      <input
-        type="date"
-        name="dateOfBirth"
-        required
-        value={birth_date}
-        onChange={event => setBirth_date(event.target.value)}
-        id={birth_dateId}
-      />
+        <label htmlFor={birth_dateId}>Date of birth</label>
+        <input
+          type="date"
+          name="dateOfBirth"
+          required
+          value={birth_date}
+          onChange={event => setBirth_date(event.target.value)}
+          id={birth_dateId}
+        />
 
-      <label htmlFor={genderId}>Your gender</label>
-      <select
-        name="gender"
-        id={genderId}
-        value={gender}
-        onChange={event => setGender(event.target.value)}
-      >
-        <option value="male">male</option>
-        <option value="female">female</option>
+        <label htmlFor={genderId}>Your gender</label>
+        <select
+          name="gender"
+          id={genderId}
+          value={gender}
+          onChange={event => setGender(event.target.value)}
+        >
+          <option value="male">male</option>
+          <option value="female">female</option>
 
-      </select>
+        </select>
 
-      <label htmlFor={jobId}>Job</label>
-      <input
-        type="text"
-        name="job"
-        required
-        placeholder="Your job"
-        value={job}
-        onChange={event => setJob(event.target.value)}
-        id={jobId}
-      />
+        <label htmlFor={jobId}>Job</label>
+        <input
+          type="text"
+          name="job"
+          required
+          placeholder="Your job"
+          value={job}
+          onChange={event => setJob(event.target.value)}
+          id={jobId}
+        />
 
-      <label htmlFor={biographyId}>Biography</label>
-      <input
-        type="text"
-        name="biography"
-        required
-        placeholder="Your biography"
-        value={biography}
-        onChange={event => setBiography(event.target.value)}
-        id={biographyId}
-      />
+        <label htmlFor={biographyId}>Biography</label>
+        <input
+          type="text"
+          name="biography"
+          required
+          placeholder="Your biography"
+          value={biography}
+          onChange={event => setBiography(event.target.value)}
+          id={biographyId}
+        />
 
-      <label htmlFor={is_activeId}>Enabled</label>
-      <input
-        type="checkbox"
-        name="isActive"
+        <label htmlFor={is_activeId}>Enabled</label>
+        <input
+          type="checkbox"
+          name="isActive"
 
-        value={is_active}
-        onChange={event => setIs_active(event.target.value)}
-        id={is_activeId}
-      />
+          value={is_active}
+          onChange={event => setIs_active(event.target.value)}
+          id={is_activeId}
+        />
 
 
-      <button type="submit">Add</button>
+        <button type="submit">Add</button>
 
-    </form >
+      </form >
+    </div>
   )
 
 }
