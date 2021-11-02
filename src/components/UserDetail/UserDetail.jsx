82e5014579ac4eed4eff38a326f82424fe318bd0
RinaOcean <antyfifa@gmail.com>
2021-11-02 13:21:19 +0200

    delete comments

--- src/components/UserDetail/UserDetail.jsx
+++ src/components/UserDetail/UserDetail.jsx
@@ -22,12 +22,10 @@ export default function UserDetail({user}) {
  
   const onDelete = id => dispatch(removeUser(id));
   const onEdit = id => dispatch(editUser(id))
-  
+
   const { id, first_name, last_name, birth_date, gender, job, biography, is_active } = user
   const enabled = String(is_active)
-  // return (
-  // <>
-  //   {users.map(({ id, first_name, last_name, birth_date, gender, job, biography, is_active }) => {
+ 
   return (
     <li key={id} className="UsersListItem">
       <p className="UsersListItemInfo">First name: {first_name}</p>
@@ -40,10 +38,7 @@ export default function UserDetail({user}) {
       <button type="button" onClick={() => onEdit(id)}>Edit</button>
       <button type="button" onClick={() => onDelete(id)}>Delete</button>
     </li>
-    //     );
-    //   })
-    //   }
-    // </>
+   
   );
 };
 
